81dbc875df3c1c19edf29837cf472138a3cd2bc7
Justin Daniel <justinddaniel@gmail.com>
2017-11-28 01:16:44 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,12 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(song, artist)
+    if !song.artist.nil?
+      link_to(artist.name, artist_path(artist))
+    else
+      display_artist(song)
+    end
+  end
 end
